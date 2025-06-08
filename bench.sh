6c83043adadf01eadcc98abfeec8e5e167582456
go-while <go-while@users.noreply.github.com>
2025-06-08 22:59:34 +0100

    Create bench.sh

---
+++ bench.sh
@@ -0,0 +1,5 @@
+build/rapidyenc_bench --bench encode,decode,crc --threads 4 --reps 2500
+build/rapidyenc_bench --bench encode --threads 4 --reps 2500
+build/rapidyenc_bench --bench decode --threads 4 --reps 2500
+build/rapidyenc_bench --bench crc --threads 4 --reps 2500
+build/rapidyenc_bench --bench decode,encode --threads 4 --reps 2500
